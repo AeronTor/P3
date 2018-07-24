dd44ba9e1a1efc08bbd85aa36c5432ce2f5947a1
AeronTor <aarontornes@gmail.com>
2018-07-24 15:19:19 -0400

    Added database initializer with seed data

---
+++ P3/Data/ContactInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using P3.Models;
+
+namespace P3.Data
+{
+    public class ContactInitializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        //Initialize database with seed data
+        protected override void Seed(Context context)
+        {
+            var contact1 = new Contact()
+            {
+                ID = 1,
+                FirstName = "Tony",
+                LastName = "Smith",
+                Email = "tonysmith@hotmail.com"
+            };
+
+            var contact2 = new Contact()
+            {
+                ID = 2,
+                FirstName = "Sally",
+                LastName = "Roger",
+                Email = "sroger01@hotmail.com"
+            };
+
+            var contact3 = new Contact()
+            {
+                ID = 3,
+                FirstName = "Crazy",
+                LastName = "Eighty-Eight",
+                Email = "thecrazyeightyeight@gmail.com"
+            };
+
+            var contact4 = new Contact()
+            {
+                ID = 4,
+                FirstName = "Tomy",
+                LastName = "Smith",
+                Email = "tomysmith@hotmail.com"
+            };
+        }
+    }
+}
