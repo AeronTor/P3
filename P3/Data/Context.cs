f939b9a1a12e3a7d38cad6ba5a3d28837f8b7c62
AeronTor <aarontornes@gmail.com>
2018-07-24 15:09:46 -0400

    Added Context class

---
+++ P3/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using P3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace P3.Data
+{
+    public class Context : DbContext
+    {
+       public DbSet<Contact> Contacts { get; set; }
+    }
+}
